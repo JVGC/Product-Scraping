afbc93fd1dfc54905eeaed363047f5bf59f3a21e
joaovictorgarciacoelho@gmail.com <joaovictorgarciacoelho@gmail.com>
2023-06-19 11:00:20 -0300

    Setting up tags for docs

--- src/django_root/views.py
+++ src/django_root/views.py
@@ -1,11 +1,13 @@
 """ Django Root Views """
 from rest_framework import views
 from rest_framework.response import Response
+from drf_spectacular.utils import extend_schema
 
 
 class HomeView(views.APIView):
     """Django Home View"""
 
+    @extend_schema(tags=["Home"])
     def get(self, _) -> Response:
         """GET Home Method
         Returns the message: Fullstack Challenge 20201026
--- src/products/views.py
+++ src/products/views.py
@@ -1,10 +1,12 @@
 """ Products Django App Views """
 
 from rest_framework import generics
+from drf_spectacular.utils import extend_schema
 from .models import Product
 from .serializers import ProductSerializer
 
 
+@extend_schema(tags=["Products"])
 class GetProductByCode(generics.RetrieveAPIView):
     """Get a product by code"""
 
@@ -13,6 +15,7 @@ class GetProductByCode(generics.RetrieveAPIView):
     lookup_field = "code"
 
 
+@extend_schema(tags=["Products"])
 class ListProducts(generics.ListAPIView):
     """List all the products by page"""
 
